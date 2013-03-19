3d083ce49dbbde5dfa1cabc3f631e96cf52d3895
Scott Ayliffe <scottjayliffe@gmail.com>
2013-03-19 05:33:42 -0700

    I made it so anyone can delete

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -75,7 +75,7 @@ class PinsController < ApplicationController
   # DELETE /pins/1
   # DELETE /pins/1.json
   def destroy
-    @pin = current_user.pins.find(params[:id])
+    @pin = Pin.find(params[:id])
     @pin.destroy
 
     respond_to do |format|

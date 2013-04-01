366f322719bb8540db03099d8cf884afe6cdcdce
Scott Ayliffe <scottjayliffe@gmail.com>
2013-04-01 02:29:37 -0700

    Add Image Uploading via URL

--- db/migrate/20130401092225_add_image_remote_url_to_pins.rb
+++ db/migrate/20130401092225_add_image_remote_url_to_pins.rb
@@ -1,9 +1,5 @@
 class AddImageRemoteUrlToPins < ActiveRecord::Migration
-  def self.up
+  def change
     add_column :pins, :image_remote_url, :string
-  end
-
-  def self.down
-    remove_column :pins, :image_remote_url
   end
 end

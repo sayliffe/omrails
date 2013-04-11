622623a1854e9bf2aa2b09d3bea5ac2da1b1afcc
Scott Ayliffe <scottjayliffe@gmail.com>
2013-04-11 05:32:04 -0700

    twitter credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -214,7 +214,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_"]
+  config.omniauth :twitter, ENV["CI5l7CM07YG6HkIUURHAg"], ENV["4k6Nq8MyJ2VcJytP81R8xnKJlCspTMDOGrsn6LNZFro"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
